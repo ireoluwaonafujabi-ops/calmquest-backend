3e7aa5360de28baf005deae8d3d240ba7ac0b868
ireoluwaonafujabi-ops <ireoluwaonafujabi@gmail.com>
2026-05-27 14:46:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-// SIMPLE "DATABASE" (in memory)
-let appointments = [];
+// CONNECT TO MONGODB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+// SCHEMA
+const appointmentSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String
+});
+
+// MODEL
+const Appointment = mongoose.model("Appointment", appointmentSchema);
 
 // SAVE DATA
-app.post("/api/appointments", (req, res) => {
-  appointments.push(req.body);
+app.post("/api/appointments", async (req, res) => {
+  const newAppointment = new Appointment(req.body);
+  await newAppointment.save();
+
   res.json({ message: "Saved successfully" });
 });
 
 // GET DATA
-app.get("/api/appointments", (req, res) => {
+app.get("/api/appointments", async (req, res) => {
+  const appointments = await Appointment.find();
   res.json(appointments);
 });
 
@@ -29,5 +46,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
